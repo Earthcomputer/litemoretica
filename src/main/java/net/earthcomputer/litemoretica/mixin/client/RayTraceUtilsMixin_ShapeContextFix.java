f68bc125c672b642ca6d80c6468169b259d3c273
Joe <burtonjae@hotmail.co.uk>
2023-06-04 12:54:19 +0100

    Add missing code

--- src/main/java/net/earthcomputer/litemoretica/mixin/client/RayTraceUtilsMixin_ShapeContextFix.java
+++ src/main/java/net/earthcomputer/litemoretica/mixin/client/RayTraceUtilsMixin_ShapeContextFix.java
@@ -26,37 +26,37 @@ public class RayTraceUtilsMixin_ShapeContextFix {
     @Nullable
     private static WeakReference<Entity> litemoretica_currentEntity;
 
-    @Inject(method = "traceToSchematicWorld", at = @At(value = "INVOKE", target = "Lfi/dy/masa/litematica/util/RayTraceUtils;rayTraceBlocks(Lnet/minecraft/world/World;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/world/RaycastContext$FluidHandling;ZZZI)Lnet/minecraft/util/hit/BlockHitResult;"))
+    @Inject(method = "traceToSchematicWorld", at = @At(value = "INVOKE", target = "Lfi/dy/masa/litematica/util/RayTraceUtils;rayTraceBlocks(Lnet/minecraft/world/World;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/world/RaycastContext$FluidHandling;ZZZI)Lnet/minecraft/util/hit/BlockHitResult;", remap = true))
     private static void traceToSchematicWorld_preRayTraceBlocks(Entity entity, double range, boolean respectRenderRange, boolean targetFluids, CallbackInfoReturnable<BlockHitResult> cir) {
         litemoretica_currentEntity = new WeakReference<>(entity);
     }
 
-    @Inject(method = "traceToSchematicWorld", at = @At(value = "INVOKE", target = "Lfi/dy/masa/litematica/util/RayTraceUtils;rayTraceBlocks(Lnet/minecraft/world/World;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/world/RaycastContext$FluidHandling;ZZZI)Lnet/minecraft/util/hit/BlockHitResult;", shift = At.Shift.AFTER))
+    @Inject(method = "traceToSchematicWorld", at = @At(value = "INVOKE", target = "Lfi/dy/masa/litematica/util/RayTraceUtils;rayTraceBlocks(Lnet/minecraft/world/World;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/world/RaycastContext$FluidHandling;ZZZI)Lnet/minecraft/util/hit/BlockHitResult;", shift = At.Shift.AFTER, remap = true))
     private static void traceToSchematicWorld_postRayTraceBlocks(CallbackInfoReturnable<BlockHitResult> cir) {
         litemoretica_currentEntity = null;
     }
 
-    @Inject(method = "getFurthestSchematicWorldBlockBeforeVanilla", at = @At(value = "INVOKE", target = "Lfi/dy/masa/litematica/util/RayTraceUtils;rayTraceBlocksToList(Lnet/minecraft/world/World;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/world/RaycastContext$FluidHandling;ZZZI)Ljava/util/List;"))
+    @Inject(method = "getFurthestSchematicWorldBlockBeforeVanilla", at = @At(value = "INVOKE", target = "Lfi/dy/masa/litematica/util/RayTraceUtils;rayTraceBlocksToList(Lnet/minecraft/world/World;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/world/RaycastContext$FluidHandling;ZZZI)Ljava/util/List;", remap = true))
     private static void getFurthestSchematicWorldBlockBeforeVanilla_preRayTraceBlocksToList(World worldClient, Entity entity, double maxRange, boolean requireVanillaBlockBehind, CallbackInfoReturnable<BlockPos> cir) {
         litemoretica_currentEntity = new WeakReference<>(entity);
     }
 
-    @Inject(method = "getFurthestSchematicWorldBlockBeforeVanilla", at = @At(value = "INVOKE", target = "Lfi/dy/masa/litematica/util/RayTraceUtils;rayTraceBlocksToList(Lnet/minecraft/world/World;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/world/RaycastContext$FluidHandling;ZZZI)Ljava/util/List;", shift = At.Shift.AFTER))
+    @Inject(method = "getFurthestSchematicWorldBlockBeforeVanilla", at = @At(value = "INVOKE", target = "Lfi/dy/masa/litematica/util/RayTraceUtils;rayTraceBlocksToList(Lnet/minecraft/world/World;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/world/RaycastContext$FluidHandling;ZZZI)Ljava/util/List;", shift = At.Shift.AFTER, remap = true))
     private static void getFurthestSchematicWorldBlockBeforeVanilla_postRayTraceBlocksToList(CallbackInfoReturnable<BlockPos> cir) {
         litemoretica_currentEntity = null;
     }
 
-    @Inject(method = "getRayTraceFromEntity", at = @At(value = "INVOKE", target = "Lfi/dy/masa/litematica/util/RayTraceUtils;rayTraceBlocks(Lnet/minecraft/world/World;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/world/RaycastContext$FluidHandling;ZZZI)Lnet/minecraft/util/hit/BlockHitResult;"))
+    @Inject(method = "getRayTraceFromEntity", at = @At(value = "INVOKE", target = "Lfi/dy/masa/litematica/util/RayTraceUtils;rayTraceBlocks(Lnet/minecraft/world/World;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/world/RaycastContext$FluidHandling;ZZZI)Lnet/minecraft/util/hit/BlockHitResult;", remap = true))
     private static void getRayTraceFromEntity_preRayTraceBlocks(World world, Entity entity, boolean useLiquids, double range, CallbackInfoReturnable<HitResult> cir) {
         litemoretica_currentEntity = new WeakReference<>(entity);
     }
 
-    @Inject(method = "getRayTraceFromEntity", at = @At(value = "INVOKE", target = "Lfi/dy/masa/litematica/util/RayTraceUtils;rayTraceBlocks(Lnet/minecraft/world/World;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/world/RaycastContext$FluidHandling;ZZZI)Lnet/minecraft/util/hit/BlockHitResult;", shift = At.Shift.AFTER))
+    @Inject(method = "getRayTraceFromEntity", at = @At(value = "INVOKE", target = "Lfi/dy/masa/litematica/util/RayTraceUtils;rayTraceBlocks(Lnet/minecraft/world/World;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/world/RaycastContext$FluidHandling;ZZZI)Lnet/minecraft/util/hit/BlockHitResult;", shift = At.Shift.AFTER, remap = true))
     private static void getRayTraceFromEntity_postRayTraceBlocks(CallbackInfoReturnable<HitResult> cir) {
         litemoretica_currentEntity = null;
     }
 
-    @Redirect(method = {"traceFirstStep", "traceLoopSteps"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getOutlineShape(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/util/shape/VoxelShape;"), require = -1)
+    @Redirect(method = {"traceFirstStep", "traceLoopSteps"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getOutlineShape(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/util/shape/VoxelShape;", remap = true), require = -1)
     private static VoxelShape redirectGetOutlineShape(BlockState instance, BlockView blockView, BlockPos blockPos) {
         Entity currentEntity;
         if (litemoretica_currentEntity != null && (currentEntity = litemoretica_currentEntity.get()) != null) {
--- src/main/java/net/earthcomputer/litemoretica/mixin/client/WorldUtilsMixin_EasyPlaceFix.java
+++ src/main/java/net/earthcomputer/litemoretica/mixin/client/WorldUtilsMixin_EasyPlaceFix.java
@@ -2,10 +2,12 @@ package net.earthcomputer.litemoretica.mixin.client;
 
 import fi.dy.masa.litematica.util.WorldUtils;
 import fi.dy.masa.malilib.util.LayerRange;
+import net.earthcomputer.litemoretica.client.EasyPlaceFix;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
@@ -16,8 +18,18 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin(WorldUtils.class)
+@Mixin(value = WorldUtils.class, remap = false)
 public class WorldUtilsMixin_EasyPlaceFix {
+    @Inject(method = "doEasyPlaceAction", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerInteractionManager;interactBlock(Lnet/minecraft/client/network/ClientPlayerEntity;Lnet/minecraft/util/Hand;Lnet/minecraft/util/hit/BlockHitResult;)Lnet/minecraft/util/ActionResult;", remap = true), require = 2)
+    private static void preInteractBlock(CallbackInfoReturnable<ActionResult> cir) {
+        EasyPlaceFix.isPlacingWithEasyPlace = true;
+    }
+
+    @Inject(method = "doEasyPlaceAction", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerInteractionManager;interactBlock(Lnet/minecraft/client/network/ClientPlayerEntity;Lnet/minecraft/util/Hand;Lnet/minecraft/util/hit/BlockHitResult;)Lnet/minecraft/util/ActionResult;", shift = At.Shift.AFTER, remap = true), require = 2)
+    private static void postInteractBlock(CallbackInfoReturnable<ActionResult> cir) {
+        EasyPlaceFix.isPlacingWithEasyPlace = false;
+    }
+
     @Inject(method = "placementRestrictionInEffect", at = @At(value = "INVOKE", target = "Lfi/dy/masa/litematica/materials/MaterialCache;getInstance()Lfi/dy/masa/litematica/materials/MaterialCache;"), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
     private static void stopEasyPlaceWhenBlockAlreadyCorrect(MinecraftClient mc, CallbackInfoReturnable<Boolean> cir, HitResult trace, ItemStack stack, BlockHitResult blockHitResult, ItemPlacementContext ctx, BlockPos pos, BlockState stateClient, World worldSchematic, LayerRange range, boolean schematicHasAir, BlockState stateSchematic) {
         if (stateClient == stateSchematic) {
